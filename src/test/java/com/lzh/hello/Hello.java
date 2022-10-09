557f07e3930d86f90cbf8535c98a9896f2979162
lin <2961348351@qq.com>
2022-10-09 22:57:46 +0800

    third-commit

--- src/test/java/com/lzh/hello/Hello.java
+++ src/test/java/com/lzh/hello/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("你好呀，兄弟门");
         System.out.println("陈冰茹,GUNDUZI");
+        System.out.println("陈冰茹,滚犊子");
     }
 }
