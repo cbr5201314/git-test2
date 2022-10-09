35c61d1144feda0c3d2dae755c5e9ecb9b72b0b9
lin <2961348351@qq.com>
2022-10-09 23:35:08 +0800

    six-commit

--- src/test/java/com/lzh/hello/cbr.java
+++ src/test/java/com/lzh/hello/cbr.java
@@ -6,5 +6,6 @@ package com.lzh.hello;
 public class cbr {
     public static void main(String[] args) {
         System.out.println("你再给我生气一次式式");
+        System.out.println("你再给我生气一次式式");
     }
 }
