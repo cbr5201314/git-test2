4bcb3ed33b96c3f0dccb17b427d90b37ab04a11c
cbr5201314 <115406062+cbr5201314@users.noreply.github.com>
2022-10-09 23:38:32 +0800

    pull commit

--- src/test/java/com/lzh/hello/cbr.java
+++ src/test/java/com/lzh/hello/cbr.java
@@ -7,5 +7,6 @@ public class cbr {
     public static void main(String[] args) {
         System.out.println("你再给我生气一次式式");
         System.out.println("你再给我生气一次式式");
+         System.out.println("你再给我生气一次式式");
     }
 }
